868597ad3e5fef107d64a7f101471d46fbf93d46
Michael Moorley <moorleymichael@gmail.com>
2022-05-19 08:00:49 -0400

    005 Updated CSS to SCSS Files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './routes/index/home.component';
 import Projects from './routes/projects/projects.component';
 import Monsters from './routes/projects/monsters-rolodex/monsters-rolodex.component';
 import Shop from './routes/projects/shop/shop.component';
-import './App.css';
+import './App.scss';
 
 const App = () => {
   return (
